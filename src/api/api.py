64934a2599738f05910693202452c971bf9f810b
LukasD <410034@mail.muni.cz>
2023-12-04 17:51:54 +0100

    added basic error logs to API

--- src/api/api.py
+++ src/api/api.py
@@ -1,3 +1,5 @@
+import logging
+
 from src.analysis_input.analysis_types import AnalysisType
 from src.analysis_input.input import Input
 from src.api.model import AnalysisModel
@@ -5,19 +7,23 @@ from src.parser.parser import Parser
 from src.analyzer.analyzer import Analyzer
 from src.analysis_output.output import OutputEncoder
 
+from sys import stdout
 from base64 import b64decode
 from json import dumps
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import Response
 from fastapi.middleware.cors import CORSMiddleware
 
+# Setup logging
+logging.basicConfig(stream=stdout)
+
+# Setup API
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_methods=["POST"]
 )
-
 
 @app.post("/analysis")
 async def execute_analysis(analysis: AnalysisModel):
@@ -26,6 +32,7 @@ async def execute_analysis(analysis: AnalysisModel):
         parser = Parser()
         bpmn4frss_elements = parser.parse_string(b64decode(analysis.model))
     except Exception as e:
+        logging.error('Error while parsing the BPMN4FRSS model: %s', e)
         raise HTTPException(status_code=400, detail=f"Error while parsing the BPMN4FRSS model: {e}")
 
     try:
@@ -33,4 +40,5 @@ async def execute_analysis(analysis: AnalysisModel):
         json_output = dumps(analysis_output, cls=OutputEncoder).encode('utf-8')
         return Response(media_type="application/json", content=json_output)
     except Exception as e:
+        logging.error('Analysis error: %s', e)
         raise HTTPException(status_code=500, detail=f"Analysis error: {e}")
